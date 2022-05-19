3471a36c0b26a8391849b271a4b148680eca2ec6
Strajde <patrykgorny1@gmail.com>
2022-05-19 20:42:31 +0200

    Mover + changes in inheritance

---
+++ Mover.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mover : Fighter
+{
+    protected BoxCollider2D boxCollider;
+    protected Vector3 moveDelta;
+    protected RaycastHit2D hit;
+    protected float xSpeed = 1.0f;
+    protected float ySpeed = 0.75f;
+
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");       
+    }
+
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+        // Reset Movedelta - bo x i y i tak s¹ w inpucie i s¹ zawsze po framie 0, -1 albo 1. Reset co frame
+        moveDelta = input;
+
+        // Swap sprite direction, wether yo're going right or left
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Make sure we can move by casting a box. If the box returns 0, we're free to move.
+        // 0 to rotacja - w 2d - zerowa
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make this thing move!
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make this thing move!
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+    }
+}
--- Player.cs
+++ Player.cs
@@ -4,46 +4,5 @@ using UnityEngine;
 
 public class Player : Fighter
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
-
-    private void Start()
-    {
-        boxCollider = GetComponent<BoxCollider2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        // Reset Movedelta - bo x i y i tak s¹ w inpucie i s¹ zawsze po framie 0, -1 albo 1. Reset co frame
-        moveDelta = new Vector3(x,y,0);
-
-        // Swap sprite direction, wether yo're going right or left
-        if (moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-
-        // Make sure we can move by casting a box. If the box returns 0, we're free to move.
-        // 0 to rotacja - w 2d - zerowa
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            // Make this thing move!
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        }
-        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            // Make this thing move!
-            transform.Translate(moveDelta.x * Time.deltaTime,0,0);
-        }
-
-
-
-
-    }
+    
 }
